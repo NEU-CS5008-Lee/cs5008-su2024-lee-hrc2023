ea785bf49853fc868a98db19f96640f6d0da7f21
hrc2023 <156156731+hrc2023@users.noreply.github.com>
2024-05-10 10:46:17 -0700

    Update helloworld.c

--- assignment1/helloworld.c
+++ assignment1/helloworld.c
@@ -4,7 +4,7 @@
 // email: jeo.lee@northeastern.edu
 
 int main() {
-  printf("Hello World.  This is [YourName].\n");
-  printf("I am assignment 1 for [This Semester].\n");
+  printf("Hello World.  This is Haoyang (Hendrick) Chen.\n");
+  printf("I am assignment 1 for Summer 2024.\n");
   return 0;
 }
