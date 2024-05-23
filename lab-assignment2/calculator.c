5e2487e244424ef86f63169066c842ba73178ed8
hrc2023 <156156731+hrc2023@users.noreply.github.com>
2024-05-23 03:51:59 -0700

    Add files via upload

--- lab-assignment2/calculator.c
+++ lab-assignment2/calculator.c
@@ -1,18 +1,38 @@
-//enter your name here
-//enter your email here
+// Haoyang (Hendrick) Chen
+// chen.haoyang4@northeastern.edu
 #include<stdio.h>
 int main()
 {
     int a,b,c, choice;
     
     printf("Enter your choice\n");
-    printf(" 1.addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
+    printf(" 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
     scanf("%d",&choice);
     
     printf("Enter a and b values");
     scanf("%d %d", &a, &b);
     
     // Add your code here using switch
+    switch (choice) {
+        case 1: // numbers need no '' or ""
+        printf("%d + %d = %d\n", a, b, a + b);
+        break;
+        case 2:
+        printf("%d - %d = %d\n", a, b, a - b);
+        break;
+        case 3:
+        printf("%d * %d = %d\n", a, b, a * b);
+        break;
+        case 4:
+        if (b != 0.0)
+            printf("%d / %d = %d\n", a, b, a / b);
+        else
+            printf("Division by Zero Error! \n");
+        break;
+    default:
+        printf("Choose a valid option! \n");
+        break;
+    }
            
     return 0;
 }
--- lab-assignment2/employee.c
+++ lab-assignment2/employee.c
@@ -14,12 +14,29 @@ typedef struct employee
 void readData(int n, employee_t* s)
 {
     // Add your code here
+    for (int i = 0; i < n; i++) {
+        printf("Enter details for employee %d\n", i + 1);
+        printf("Employee ID: ");
+        scanf("%d", &s[i].empId);
+        printf("Name: ");
+        scanf("%s", s[i].Name);
+        printf("Designation: ");
+        scanf("%s", s[i].Designation);
+        printf("Department: ");
+        scanf("%s", s[i].Dept);
+    }
 }
 /* Function to print the employee details*/
 void display(int n, employee_t * s)
 {
     // Add your code here
-    
+    for (int i = 0; i < n; i++) {
+        printf("\nDetails of employee %d\n", i + 1);
+        printf("Employee ID: %d\n", s[i].empId);
+        printf("Name: %s\n", s[i].Name);
+        printf("Designation: %s\n", s[i].Designation);
+        printf("Department: %s\n", s[i].Dept);
+    }
 }
 
 /*----------Main Program--------*/
@@ -28,7 +45,29 @@ int main()
 {
     // Main Function to print the employee details
     // Add your code here
-    
+    int n;
+    employee_t* empteam1;
 
+    // Define the number of employees
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+    
+    // Allocating memory with size of one employee multiply the 
+    // number of employees
+    empteam1 = (employee_t*)malloc(n * sizeof(employee_t));
+
+    if (empteam1 == NULL) {
+        printf("Memory allocation failed\n");
+        return 1; // Exit the program if memory allocation fails
+    }
+    
+    readData(n, empteam1);
+
+    
+    display(n, empteam1);
+    
+    free(empteam1);
+
+    return 0;
 }
 
