8cd6e897b9014600a0c05a663572a37e64a6b559
hrc2023 <156156731+hrc2023@users.noreply.github.com>
2024-06-06 17:54:44 -0700

    Add files via upload

--- lab-assignment4/insertion-lab.c
+++ lab-assignment4/insertion-lab.c
@@ -1,6 +1,6 @@
-//enter your name here
-//enter your email here
-//To perform insertion sort of integers stored in a file and display the output and store it in another file.
+// Haoyang (Hendrick) Chen
+// chen.haoyang4@northeastern.edu
+// To perform insertion sort of integers stored in a file and display the output and store it in another file.
 #include <stdio.h>
 #include<stdlib.h>
 int main()
@@ -11,7 +11,9 @@ int main()
     /*count will have the total number of elements in the array*/
 
     /*read the file*/
-    fp=fopen("Input.txt","r");
+    // Add: You need to specify the path in the first run
+    // otherwise it won't open!!!
+    fp=fopen("Input1.txt","r");
     if(fp==NULL){
         //if file returns NULL then can't open the file
         printf("\n Cannot open the file \n");
@@ -19,12 +21,42 @@ int main()
     }
     
     //insert your code here
-    
-    
-    
-    
-    
-    
-    
+        /* Read integers from the file */
+    while (fscanf(fp, "%d", &arr[count]) != EOF) {
+        count++;
+    }
+    fclose(fp);
+
+    /* Perform insertion sort */
+    for (i = 1; i < count; i++) {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = temp;
+    }
+
+    /* Display the sorted array */
+    printf("Sorted array:\n");
+    for (i = 0; i < count; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    /* Write the sorted integers to the output file */
+    fp = fopen("Output.txt", "w");
+    if (fp == NULL) {
+        // If file returns NULL then can't open the file
+        printf("\n Cannot open the file \n");
+        exit(0);
+    }
+
+    for (i = 0; i < count; i++) {
+        fprintf(fp, "%d ", arr[i]);
+    }
+    fclose(fp);
+
     return 0;
 }
