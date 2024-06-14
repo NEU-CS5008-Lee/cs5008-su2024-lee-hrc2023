47766e0b70d17b48e4ec1e565f89c166213343f0
hrc2023 <156156731+hrc2023@users.noreply.github.com>
2024-06-13 17:52:10 -0700

    Add files via upload

--- lab-assignment5/reversell-recur-lab.c
+++ lab-assignment5/reversell-recur-lab.c
@@ -1,4 +1,5 @@
-//enter your name and email here
+// Hendrick Chen
+// chen.haoyang4@northeastern.edu
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct node
@@ -43,12 +44,17 @@ void display(List* l)
 /*-------reversing the linked list using recursion------*/
 void reverse(List* l, node_t* ptr)
 {
-    
-    
-    
-    
-//insert your code here
-    
+    //insert your code here
+    if (ptr == NULL || ptr->next == NULL) {
+        l->head = ptr; // Set the current node as the head of the list
+        return;
+    }
+
+    reverse(l, ptr->next); // Recursion
+
+    ptr->next->next = ptr; // Makes the next node's next pointer point
+    // to the current node
+    ptr->next = NULL;  // Break the old next node
     
 }
 
